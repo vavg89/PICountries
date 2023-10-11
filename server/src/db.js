a3854c263708e7748da322c48ef76a7293734240
vavg89 <109152724+vavg89@users.noreply.github.com>
2023-10-10 22:53:00 -0300

    readme
    readme

--- server/src/db.js
+++ server/src/db.js
@@ -4,10 +4,10 @@ require('dotenv').config()
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, 
   native: false, 
 });
